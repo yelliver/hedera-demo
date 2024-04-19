e0b0816ca3381ac4309dd835df2b4b534879b6bf
GiAnG <hoang.giang.do.tien@autodesk.com>
2024-04-19 15:56:12 +0800

    test

--- src/main/java/com/gft/demo/hedera/AccountClient.java
+++ src/main/java/com/gft/demo/hedera/AccountClient.java
@@ -1,82 +1,158 @@
 package com.gft.demo.hedera;
 
 import com.hedera.hashgraph.sdk.*;
+import lombok.SneakyThrows;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.concurrent.TimeoutException;
 
 public class AccountClient implements AutoCloseable {
-    private final AccountId mainAccountId;
-    private final PrivateKey mainPrivateKey;
-    private final Client client = Client.forTestnet();
+  private final AccountId mainAccountId;
+  private final PrivateKey mainPrivateKey;
+  private final Client client = Client.forTestnet();
 
-    public AccountClient(AccountId mainAccountId, PrivateKey mainPrivateKey) {
-        this.mainAccountId = mainAccountId;
-        this.mainPrivateKey = mainPrivateKey;
+  public AccountClient(AccountId mainAccountId, PrivateKey mainPrivateKey) {
+    this.mainAccountId = mainAccountId;
+    this.mainPrivateKey = mainPrivateKey;
 
-        client.setOperator(mainAccountId, mainPrivateKey);
-        client.setDefaultMaxTransactionFee(new Hbar(100));
-        client.setMaxQueryPayment(new Hbar(50));
+    client.setOperator(mainAccountId, mainPrivateKey);
+    client.setDefaultMaxTransactionFee(new Hbar(100));
+    client.setMaxQueryPayment(new Hbar(50));
+  }
+
+  public static AccountClient fromCred(AccountCred accountCred) {
+    return new AccountClient(accountCred.getAccountId(), accountCred.getPrivateKey());
+  }
+
+  public AccountBalance balanceQuery() throws PrecheckStatusException, TimeoutException {
+    return new AccountBalanceQuery()
+      .setAccountId(mainAccountId)
+      .execute(client);
+  }
+
+  public AccountBalance balanceQuery(AccountId accountId) throws PrecheckStatusException, TimeoutException {
+    return new AccountBalanceQuery()
+      .setAccountId(accountId)
+      .execute(client);
+  }
+
+  @SneakyThrows
+  public AccountCred createAccount(int balance) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+    var privateKey = PrivateKey.generateED25519();
+    var publicKey = privateKey.getPublicKey();
+    AccountCreateTransaction transaction = new AccountCreateTransaction()
+      .setKey(publicKey)
+      .setInitialBalance(Hbar.fromTinybars(balance));
+
+    findMethod(transaction, "mergeFromClient", Client.class).invoke(transaction, client);
+    findMethod(transaction, "onExecute", Client.class).invoke(transaction, client);
+    findMethod(transaction, "checkNodeAccountIds").invoke(transaction);
+    findMethod(transaction, "setNodesFromNodeAccountIds", Client.class).invoke(transaction, client);
+
+//    mergeFromClient(client);
+//    onExecute(client);
+//    checkNodeAccountIds();
+//    setNodesFromNodeAccountIds(client);
+//    @Nullable Network network, int attempt, Duration grpcDeadline
+    //            GrpcRequest grpcRequest = new GrpcRequest(client.network, attempt, currentTimeout);
+    Class grpcRequestClass = findNested(transaction, "GrpcRequest");
+    Class<?> networkClass = Class.forName("com.hedera.hashgraph.sdk.Network");
+    System.out.println(grpcRequestClass);
+    System.out.println(networkClass);
+
+    Object network = findField(client, "network");
+    System.out.println(network);
+
+//    System.out.println(findNested);
+    Constructor declaredConstructor1 = grpcRequestClass.getDeclaredConstructors()[0];
+    declaredConstructor1.setAccessible(true);
+    Object o = declaredConstructor1.newInstance(transaction,network, 1, client.getRequestTimeout());
+//    Constructor declaredConstructor = grpcRequestClass.getDeclaredConstructor(Executable.class, networkClass, int.class, Duration.class);
+//    Object o = declaredConstructor.newInstance();
+//    System.out.println(o);
+    var newAccountTx = transaction.execute(client);
+    var newAccountId = newAccountTx.getReceipt(client).accountId;
+    return new AccountCred(newAccountId, privateKey);
+  }
+
+  @SneakyThrows
+  private static Method findMethod(Object object, String name, Class<?>... parameterTypes) {
+    Class clazz = object.getClass();
+    while (true) {
+      try {
+        Method method = clazz.getDeclaredMethod(name, parameterTypes);
+        method.setAccessible(true);
+        return method;
+      } catch (NoSuchMethodException | SecurityException e) {
+        clazz = clazz.getSuperclass();
+        if (clazz == null) {
+          throw e;
+        }
+      }
     }
+  }
 
-    public static AccountClient fromCred(AccountCred accountCred) {
-        return new AccountClient(accountCred.getAccountId(), accountCred.getPrivateKey());
+  @SneakyThrows
+  private static Class findNested(Object object, String name) {
+    Class clazz = object.getClass();
+    while (true) {
+      for (Class declaredClass : clazz.getDeclaredClasses()) {
+        if (declaredClass.getName().contains(name))
+          return declaredClass;
+      }
+      clazz = clazz.getSuperclass();
+      if (clazz == null) {
+        throw null;
+      }
     }
+  }
 
-    public AccountBalance balanceQuery() throws PrecheckStatusException, TimeoutException {
-        return new AccountBalanceQuery()
-                .setAccountId(mainAccountId)
-                .execute(client);
-    }
+  @SneakyThrows
+  private static void invoke(Method method, Object obj, Object... args) {
+    method.invoke(obj, args);
+  }
 
-    public AccountBalance balanceQuery(AccountId accountId) throws PrecheckStatusException, TimeoutException {
-        return new AccountBalanceQuery()
-                .setAccountId(accountId)
-                .execute(client);
-    }
+  @SneakyThrows
+  private static Object findField(Object object, String name){
+    Field declaredField = object.getClass().getDeclaredField(name);
+    declaredField.setAccessible(true);
+    return declaredField.get(object);
+  }
 
-    public AccountCred createAccount(int balance) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
-        var privateKey = PrivateKey.generateED25519();
-        var publicKey = privateKey.getPublicKey();
-        var newAccountTx = new AccountCreateTransaction()
-                .setKey(publicKey)
-                .setInitialBalance(Hbar.fromTinybars(balance))
-                .execute(client);
-        var newAccountId = newAccountTx.getReceipt(client).accountId;
-        return new AccountCred(newAccountId, privateKey);
-    }
+  public Status sendTo(AccountId receiver, long amount) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+    var transferTx = new TransferTransaction()
+      .addHbarTransfer(this.mainAccountId, Hbar.fromTinybars(-amount))
+      .addHbarTransfer(receiver, Hbar.fromTinybars(amount))
+      .execute(client);
+    return transferTx.getReceipt(client).status;
+  }
 
-    public Status sendTo(AccountId receiver, long amount) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
-        var transferTx = new TransferTransaction()
-                .addHbarTransfer(this.mainAccountId, Hbar.fromTinybars(-amount))
-                .addHbarTransfer(receiver, Hbar.fromTinybars(amount))
-                .execute(client);
-        return transferTx.getReceipt(client).status;
-    }
+  public Status sendTokenTo(AccountId receiver, TokenId tokenId, long amount) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+    var transferTx = new TransferTransaction()
+      .addTokenTransfer(tokenId, this.mainAccountId, -amount)
+      .addTokenTransfer(tokenId, receiver, amount)
+      .execute(client);
+    return transferTx.getReceipt(client).status;
+  }
 
-    public Status sendTokenTo(AccountId receiver, TokenId tokenId, long amount) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
-        var transferTx = new TransferTransaction()
-                .addTokenTransfer(tokenId, this.mainAccountId, -amount)
-                .addTokenTransfer(tokenId, receiver, amount)
-                .execute(client);
-        return transferTx.getReceipt(client).status;
-    }
+  public TokenInfo tokenInfo(TokenId tokenId) throws PrecheckStatusException, TimeoutException {
+    return new TokenInfoQuery().setTokenId(tokenId).execute(client);
+  }
 
-    public TokenInfo tokenInfo(TokenId tokenId) throws PrecheckStatusException, TimeoutException {
-        return new TokenInfoQuery().setTokenId(tokenId).execute(client);
-    }
+  public Status associateToken(AccountCred accountCred, TokenId tokenId) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+    var transaction = new TokenAssociateTransaction()
+      .setAccountId(accountCred.getAccountId())
+      .setTokenIds(Collections.singletonList(tokenId));
+    var txResponse = transaction.freezeWith(client).sign(accountCred.getPrivateKey()).execute(client);
+    var receipt = txResponse.getReceipt(client);
+    return receipt.status;
+  }
 
-    public Status associateToken(AccountCred accountCred, TokenId tokenId) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
-        var transaction = new TokenAssociateTransaction()
-                .setAccountId(accountCred.getAccountId())
-                .setTokenIds(Collections.singletonList(tokenId));
-        var txResponse = transaction.freezeWith(client).sign(accountCred.getPrivateKey()).execute(client);
-        var receipt = txResponse.getReceipt(client);
-        return receipt.status;
-    }
-
-    @Override
-    public void close() throws Exception {
-        client.close();
-    }
+  @Override
+  public void close() throws Exception {
+    client.close();
+  }
 }
--- src/main/java/com/gft/demo/hedera/Main.java
+++ src/main/java/com/gft/demo/hedera/Main.java
@@ -17,89 +17,93 @@ public class Main {
         var demoAccountCred = AccountCred.fromEnv("DEMO_ACCOUNT_A_ID", "DEMO_PRIVATE_A_KEY");
         var myTokenId = TokenId.fromString(Dotenv.load().get("MY_TOKEN_ID"));
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            int choice;
-            do {
-                int i = 1;
-                System.out.println("\n====================MENU====================");
-                System.out.printf("%d. Show main account%n", i++);
-                System.out.printf("%d. Show sub demo account%n", i++);
-                System.out.printf("%d. Show my token info%n", i++);
+        newAccountCred = accountClient.createAccount(100);
+        System.out.println("===========================");
+        System.out.println(newAccountCred);
 
-                System.out.printf("%d. Query main account balance%n", i++);
-                System.out.printf("%d. Query demo account balance%n", i++);
-                System.out.printf("%d. Query new account balance%n", i++);
-
-                System.out.printf("%d. Transfer HBar to sub account%n", i++);
-                System.out.printf("%d. Transfer Token to sub account%n", i++);
-
-                System.out.printf("%d. Create new account%n", i++);
-                System.out.printf("%d. Transfer HBar to new account%n", i++);
-                System.out.printf("%d. Associate Token with new account%n", i++);
-                System.out.printf("%d. Transfer Token to new account%n", i++);
-                System.out.println("0. Exit");
-
-                System.out.print("Enter your choice: ");
-                choice = scanner.nextInt();
-
-                try {
-                    switch (choice) {
-                        case 1:
-                            System.out.println("Main Account: " + mainAccountCred);
-                            break;
-                        case 2:
-                            System.out.println("Sub Demo Account: " + demoAccountCred);
-                            break;
-                        case 3:
-                            var tokenInfo = accountClient.tokenInfo(myTokenId);
-                            System.out.println("My Token: " + tokenInfo);
-                            break;
-                        case 4:
-                            var mainAccountQuery = accountClient.balanceQuery();
-                            System.out.println("Main account balance: " + mainAccountQuery);
-                            break;
-                        case 5:
-                            var demoAccountQuery = accountClient.balanceQuery(demoAccountCred.getAccountId());
-                            System.out.println("Demo account balance: " + demoAccountQuery);
-                            break;
-                        case 6:
-                            var newAccountQuery = accountClient.balanceQuery(newAccountCred.getAccountId());
-                            System.out.println("New account balance: " + newAccountQuery);
-                            break;
-                        case 7:
-                            var statusHBarDemo = accountClient.sendTo(demoAccountCred.getAccountId(), 100);
-                            System.out.println("Transfer HBar result: " + statusHBarDemo);
-                            break;
-                        case 8:
-                            var statusTokenDemo = accountClient.sendTokenTo(demoAccountCred.getAccountId(), myTokenId, 100);
-                            System.out.println("Transfer Token result: " + statusTokenDemo);
-                            break;
-                        case 9:
-                            newAccountCred = accountClient.createAccount(100);
-                            System.out.println("Create account result: " + newAccountCred);
-                            break;
-                        case 10:
-                            var associateTokenResult = accountClient.associateToken(newAccountCred, myTokenId);
-                            System.out.println("Associate token result: " + associateTokenResult);
-                            break;
-                        case 11:
-                            var statusHBarNew = accountClient.sendTo(newAccountCred.getAccountId(), 100);
-                            System.out.println("Transfer HBar result: " + statusHBarNew);
-                            break;
-                        case 12:
-                            var statusTokenNew = accountClient.sendTokenTo(newAccountCred.getAccountId(), myTokenId, 100);
-                            System.out.println("Transfer Token result: " + statusTokenNew);
-                            break;
-                        case 0:
-                            System.out.println("Exiting...");
-                            break;
-                        default:
-                            System.out.println("Invalid choice. Please try again.");
-                    }
-                } catch (Exception e) {
-                    System.out.println("ERROR: " + e.getMessage());
-                }
-            } while (choice != 0);
-        }
+//        try (Scanner scanner = new Scanner(System.in)) {
+//            int choice;
+//            do {
+//                int i = 1;
+//                System.out.println("\n====================MENU====================");
+//                System.out.printf("%d. Show main account%n", i++);
+//                System.out.printf("%d. Show sub demo account%n", i++);
+//                System.out.printf("%d. Show my token info%n", i++);
+//
+//                System.out.printf("%d. Query main account balance%n", i++);
+//                System.out.printf("%d. Query demo account balance%n", i++);
+//                System.out.printf("%d. Query new account balance%n", i++);
+//
+//                System.out.printf("%d. Transfer HBar to sub account%n", i++);
+//                System.out.printf("%d. Transfer Token to sub account%n", i++);
+//
+//                System.out.printf("%d. Create new account%n", i++);
+//                System.out.printf("%d. Transfer HBar to new account%n", i++);
+//                System.out.printf("%d. Associate Token with new account%n", i++);
+//                System.out.printf("%d. Transfer Token to new account%n", i++);
+//                System.out.println("0. Exit");
+//
+//                System.out.print("Enter your choice: ");
+//                choice = scanner.nextInt();
+//
+//                try {
+//                    switch (choice) {
+//                        case 1:
+//                            System.out.println("Main Account: " + mainAccountCred);
+//                            break;
+//                        case 2:
+//                            System.out.println("Sub Demo Account: " + demoAccountCred);
+//                            break;
+//                        case 3:
+//                            var tokenInfo = accountClient.tokenInfo(myTokenId);
+//                            System.out.println("My Token: " + tokenInfo);
+//                            break;
+//                        case 4:
+//                            var mainAccountQuery = accountClient.balanceQuery();
+//                            System.out.println("Main account balance: " + mainAccountQuery);
+//                            break;
+//                        case 5:
+//                            var demoAccountQuery = accountClient.balanceQuery(demoAccountCred.getAccountId());
+//                            System.out.println("Demo account balance: " + demoAccountQuery);
+//                            break;
+//                        case 6:
+//                            var newAccountQuery = accountClient.balanceQuery(newAccountCred.getAccountId());
+//                            System.out.println("New account balance: " + newAccountQuery);
+//                            break;
+//                        case 7:
+//                            var statusHBarDemo = accountClient.sendTo(demoAccountCred.getAccountId(), 100);
+//                            System.out.println("Transfer HBar result: " + statusHBarDemo);
+//                            break;
+//                        case 8:
+//                            var statusTokenDemo = accountClient.sendTokenTo(demoAccountCred.getAccountId(), myTokenId, 100);
+//                            System.out.println("Transfer Token result: " + statusTokenDemo);
+//                            break;
+//                        case 9:
+//                            newAccountCred = accountClient.createAccount(100);
+//                            System.out.println("Create account result: " + newAccountCred);
+//                            break;
+//                        case 10:
+//                            var associateTokenResult = accountClient.associateToken(newAccountCred, myTokenId);
+//                            System.out.println("Associate token result: " + associateTokenResult);
+//                            break;
+//                        case 11:
+//                            var statusHBarNew = accountClient.sendTo(newAccountCred.getAccountId(), 100);
+//                            System.out.println("Transfer HBar result: " + statusHBarNew);
+//                            break;
+//                        case 12:
+//                            var statusTokenNew = accountClient.sendTokenTo(newAccountCred.getAccountId(), myTokenId, 100);
+//                            System.out.println("Transfer Token result: " + statusTokenNew);
+//                            break;
+//                        case 0:
+//                            System.out.println("Exiting...");
+//                            break;
+//                        default:
+//                            System.out.println("Invalid choice. Please try again.");
+//                    }
+//                } catch (Exception e) {
+//                    System.out.println("ERROR: " + e.getMessage());
+//                }
+//            } while (choice != 0);
+//        }
     }
 }
